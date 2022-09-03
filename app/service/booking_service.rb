3a4c1a600518f6d063ab689740ea740b09c42850
dineshredd16 <87149378+dineshredd16@users.noreply.github.com>
2022-09-03 12:06:41 +0530

    move code from controller to service, following skinny controller development practise

---
+++ app/service/booking_service.rb
@@ -0,0 +1,63 @@
+class BookingService < ApplicationService
+
+  def self.list(params)
+    bookings = Booking.where(created_at: Date.current.all_day).is_active
+    return bookings
+  end
+
+  def self.create(params)
+    company_service = CompanyService.find_by(company_id: params[:company_id], id: [params[:company_service_id]])
+    if company_service.present?
+        new_params = params.as_json(only: [:company_id, :company_service_id, :booking_date])
+        booking = Booking.new(new_params)
+        booking.is_active = true
+        if booking.save
+            retutn true, booking
+        else
+            return false, booking.errors.full_messages.join(', ')
+        end
+    else
+        return false,  "no service available for booking with company id : #{params[:company_id]} and company service id : #{params[:company_service_id]}"
+    end
+  end
+
+  def self.update(params)
+    new_params = params.as_json(only: [:booking_date, :is_active])
+    booking = Booking.find_by(id: params[:booking_id])
+    if booking.present?
+        if booking.update(new_params)
+            return true, booking
+        else
+            return false, booking.errors.full_messages.join(', ')
+        end
+    else
+        return false, "booking not found with id #{params[:booking_id]}"
+    end
+  end
+
+  def self.list_available_slots(params)
+    data = []
+    service = CompanyService.find_by(id: params[:company_service_id])
+    company = Company.find_by(id: params[:company_id])
+    if service.present? and company.present?
+        time = Time.now.in_time_zone("Asia/Kolkata")
+        if time.strftime("%M").to_i <= 30
+            time = time.beginning_of_hour
+        else
+            time = time.beginning_of_hour + 1.hour
+        end
+        if company.working_days.include? (time.wday)
+            while (time < (Time.now.in_time_zone("Asia/Kolkata").end_of_day))
+                if Booking.servicable_slot(time, service.id)
+                    data << time.strftime("%d-%m-%Y %H:%M")
+                end
+                time = time + 30.minutes
+            end
+        end
+        return true, data
+    else
+        return false, "company or service not found"
+    end     
+  end
+
+end
